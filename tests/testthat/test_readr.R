470c57cc0fac806e8e77ebddb5405ac18603d61d
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-09-15 23:43:45 -0700

    🛑🧪 Test `long_name_from_names()`

---
+++ tests/testthat/test_readr.R
@@ -0,0 +1,7 @@
+describe("long_name_from_names", {
+  it("First example", {
+    field_name <- c("goal","team_id","total_shots","shots_on_goal")
+    expected_long_names <- c("Goal","Team Id" ,"Total Shots", "Shots On Goal")
+    obtained_long_names <- long_name_from_names(field_name)
+  })
+})
